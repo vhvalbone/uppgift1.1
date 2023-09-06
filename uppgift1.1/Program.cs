6f44b3fb00c1d0d769bce1893b7223cf5d43128e
alsv06003 <alsv06003@E-PW0234JW>
2023-09-06 10:42:31 +0200

    Add project files.

---
+++ uppgift1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Exempel_uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva ut texten som \" och /");
+        }
+    }
+}
+
